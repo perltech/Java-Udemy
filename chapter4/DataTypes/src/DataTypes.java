5ab6560da3640a2f74b506c55fb5dfca1b9b89c0
perltech <noah.perlmutter@gmail.com>
2018-03-24 12:26:18 -0600

    add variable example

--- chapter4/DataTypes/src/DataTypes.java
+++ chapter4/DataTypes/src/DataTypes.java
@@ -4,7 +4,13 @@ public class DataTypes {
         System.out.println("Hello, Noah!");
 
         int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
         System.out.println(myFirstNumber);
         //sout: IntelliJ template for println
+
+        System.out.println("My total: " + myTotal);
     }
 }

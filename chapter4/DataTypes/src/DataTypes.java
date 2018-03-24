107495b7e799a62344f652bf1f69725afc178eff
swami <noah.perlmutter@gmail.com>
2018-03-24 12:20:40 -0600

    add int

--- chapter4/DataTypes/src/DataTypes.java
+++ chapter4/DataTypes/src/DataTypes.java
@@ -1,6 +1,10 @@
-public class Hello {
+public class DataTypes {
     public static void main(String[] args) {
+
         System.out.println("Hello, Noah!");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        //sout: IntelliJ template for println
     }
 }
-# changes to test token

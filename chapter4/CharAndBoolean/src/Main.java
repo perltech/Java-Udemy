b9c3d030d51aa9d4bbcd00f60eddd84fc88c9beb
perltech <noah.perlmutter@gmail.com>
2018-03-24 17:31:15 -0600

    char and boolean

---
+++ chapter4/CharAndBoolean/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //char width = 16 (2 bytes)
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+
+
+        char registerSymbol = '\u00AE';
+        System.out.println(registerSymbol);
+    }
+
+}

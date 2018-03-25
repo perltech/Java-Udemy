4695b463bca3caa5e76be9fff959e6ed136e065a
perltech <noah.perlmutter@gmail.com>
2018-03-25 12:35:46 -0600

    expressions

---
+++ chapter4/KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = 100 * 1.609344;
+        System.out.printf(kilometers);
+
+        // Expressions are the actual data being compared or assigned
+
+        if(kilometers == 50) {
+            System.out.println("It's 50.");
+        } else {
+            System.out.println("It's not 50.");
+        }
+    }
+}
---
+++ chapter4/Operators/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 1 + 2;
+        System.out.println("1 + 2 = " + sum);
+
+        int difference = sum - 1;
+        System.out.println("3 - 1 = " + difference);
+
+        int product = difference * 10;
+        System.out.println("2 * 10 = " + product);
+
+        int quotient = product / 5;
+        System.out.println("20 / 5 = " + quotient);
+
+        int modulus = quotient % 3;
+        System.out.println("4 % 3 = " + modulus);
+    }
+}

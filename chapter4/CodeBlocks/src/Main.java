c1e62814da9be492b48f5191f7f0d27694ddd33c
perltech <noah.perlmutter@gmail.com>
2018-03-26 11:08:06 -0600

    add methods

--- chapter4/CodeBlocks/src/Main.java
+++ chapter4/CodeBlocks/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int levelCompleted = 5;
+        int bonus = 100;
+        int highScore = calculateScore(true, 800, levelCompleted, bonus);
+
+        calculateScore(true, 10000, 8,200);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        // void keyword means don't return anything
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was: " + finalScore);
+            return finalScore;
+        }
+        return -1;
     }
 }

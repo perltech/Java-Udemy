4fa7c732768b0ea00e1246756086af3e782cc0ef
perltech <noah.perlmutter@gmail.com>
2018-03-28 12:19:24 -0600

    add animal and dog classes:

---
+++ chapter7/Inheritance/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal",1,1,5,5);
+        Dog dog = new Dog("Yorkie", 2, 25, 4, 4, 1, 20, "Shaggy");
+        dog.eat();
+    }
+}

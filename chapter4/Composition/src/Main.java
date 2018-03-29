cc0189c1637d622c27c200f3fe6d982608f2b928
perltech <noah.perlmutter@gmail.com>
2018-03-29 12:13:20 -0600

    assemble pc classes

---
+++ chapter4/Composition/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B", "Dell","240",dimensions);
+
+        Monitor theMonitor = new Monitor("27in Beast", "Acer", 27,
+        new Resolution(2540,1440));
+
+        Motherboard theMotherboard = new Motherboard("Bj-200", 4,6,"v.244");
+
+        PC thePC = new PC(theCase,theMonitor,theMotherboard);
+    }
+}
